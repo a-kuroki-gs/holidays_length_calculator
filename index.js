bc41a2243e69cf63bb2a244fbecc6dd00864d1a8
a-kuroki-gs <a.kuroki@genbasupport.com>
2023-12-20 12:13:15 +0900

    祝日と土日の休みを考慮するようにする
    まずは年末年始で実装

--- index.js
+++ index.js
@@ -81,13 +81,120 @@ async function calculateLength(firstDay, lastDay) {
   return (((lastDay - firstDay) / 86400000) + 1); 
 }
 
+async function calculateWeekendInclusiveSpan(firstDay, lastDay) {
+  // 土日休み
+  // 初日が日曜か月曜ならのばす
+  switch (firstDay.getDay()){
+    case 1:
+      firstDay.setDate(firstDay.getDate() - 2);
+      break;
+    case 0:
+      firstDay.setDate(firstDay.getDate() - 1);
+      break;
+    default:
+      firstDay;
+  }
+
+  // 最終日が金曜か土曜ならのばす
+  switch (lastDay.getDay()){
+    case 5:
+      lastDay.setDate(lastDay.getDate() + 2);
+      break;
+    case 6:
+      lastDay.setDate(lastDay.getDate() + 1);
+      break;
+    default:
+      lastDay;
+  }
+
+  return [firstDay, lastDay];
+}
+
+async function calculatePublicHolidayInclusiveSpan(firstDay, lastDay, publicHoliday, weekendOff) {
+  // 祝日休み
+  // 初日-1の日が祝日
+  if (publicHoliday.includes(firstDay.toISOString().slice(0, 10))){
+    // １日前にする
+    firstDay.setDate(firstDay.getDate() - 1);
+  }
+  // 最終日+1の日が祝日
+  if (publicHoliday.includes(lastDay.toISOString().slice(0, 10))){
+    // １日後にする
+    lastDay.setDate(lastDay.getDate() + 1);
+  }
+
+  // 延ばした日の曜日を考慮する
+  if (weekendOff == true) {
+    calculateWeekendInclusiveSpan(firstDay, lastDay);
+  }
+
+  return [firstDay, lastDay];
+}
+
+async function isWeekendOff() {
+  const { Select } = enquirer;
+  const choices = [
+                    { name: 'はい', value: true },
+                    { name: 'いいえ', value: false }
+                  ]
+
+  const prompt = new Select({
+    name: "selections",
+    type: "select",
+    multiple: false,
+    message: `土日は休みですか？`,
+    choices: choices,
+    result() {
+      return this.focused.value;
+    }
+  });
+
+  return await prompt.run();
+}
+
+async function isPublicHolidayOff() {
+  const { Select } = enquirer;
+  const choices = [
+                    { name: 'はい', value: true },
+                    { name: 'いいえ', value: false }
+                  ]
+
+  const prompt = new Select({
+    name: "selections",
+    type: "select",
+    multiple: false,
+    message: `祝日は休みですか？`,
+    choices: choices,
+    result() {
+      return this.focused.value;
+    }
+  });
+
+  return await prompt.run();
+}
+
 async function run() {
   const holidayName = await getHolidayName();
+  const weekendOff = await isWeekendOff();
+  const publicHoliday = ['2023-12-25', '2024-01-01', '2024-01-08'];
+  const publicHolidayOff = await isPublicHolidayOff();
 
   if ( holidayName == '年末年始' ) {
     console.log('年末年始が何連休になるか計算します！');
-    const firstDay = await getFirstDay();
-    const lastDay = await getLastDay();
+    let firstDay = await getFirstDay();
+    let lastDay = await getLastDay();
+
+    if (weekendOff == true && publicHolidayOff == true) {
+      [firstDay, lastDay] = await calculateWeekendInclusiveSpan(firstDay, lastDay);
+      while (publicHoliday.includes(firstDay.toISOString().slice(0, 10)) || publicHoliday.includes(lastDay.toISOString().slice(0, 10))) {
+        [firstDay, lastDay] = await calculatePublicHolidayInclusiveSpan(firstDay, lastDay, publicHoliday, weekendOff);
+      }
+    } else if (weekendOff == true) {
+      [firstDay, lastDay] = await calculateWeekendInclusiveSpan(firstDay, lastDay);
+    } else if (publicHolidayOff == true) {
+      [firstDay, lastDay] = await calculatePublicHolidayInclusiveSpan(firstDay, lastDay, publicHoliday, weekendOff);
+    }
+
     const period = await calculateLength(firstDay, lastDay);
     
     console.log(`${firstDay.getFullYear()}年の年末年始は${period}連休です！`);
